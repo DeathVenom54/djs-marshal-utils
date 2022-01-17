61406eb7df3960547910285a002b2641f77b829d
DeathVenom54 <fdragon602@gmail.com>
2022-01-17 16:41:25 +0530

    add ephemeral option

--- src/interceptors/replyWithSuccess.ts
+++ src/interceptors/replyWithSuccess.ts
@@ -5,12 +5,14 @@ import { successEmbed } from '../embeds';
  * Replies to an interaction with a success embed
  * @param {BaseCommandInteraction} interaction The interaction to reply to
  * @param {string} message The success message to display
+ * @param {boolean} ephemeral Whether to make reply ephemeral
  * @param {string} details Optional details to include in the message
  * @param {EmojiResolvable} emoji Emoji to include in the title
  */
 export const replyWithSuccess = async (
   interaction: BaseCommandInteraction,
   message: string,
+  ephemeral: boolean,
   details?: string,
   emoji?: EmojiResolvable,
 ): Promise<void> => {
