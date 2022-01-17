31a45f6f3900732191d8ddbff41657fe702113f3
DeathVenom54 <fdragon602@gmail.com>
2022-01-17 17:32:49 +0530

    remove direct exports from src/index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import embeds from './embeds';
 import interceptors from './interceptors';
 import checks from './checks';
 
-export * from './embeds';
-export * from './interceptors';
-export * from './checks';
-
 export default {
   embeds,
   interceptors,
