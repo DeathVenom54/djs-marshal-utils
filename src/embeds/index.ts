2ed3293b706e33dae60daeb787b397553f581b35
DeathVenom54 <fdragon602@gmail.com>
2022-01-17 16:13:01 +0530

    cleanup exports

--- src/embeds/index.ts
+++ src/embeds/index.ts
@@ -1,3 +1,13 @@
+import { simpleEmbed } from './simpleEmbed';
+import { successEmbed } from './successEmbed';
+import { errorEmbed } from './errorEmbed';
+
 export * from './simpleEmbed';
 export * from './successEmbed';
 export * from './errorEmbed';
+
+export default {
+  simpleEmbed,
+  successEmbed,
+  errorEmbed,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { errorEmbed, simpleEmbed, successEmbed } from './embeds';
+import embeds from './embeds';
 
 export * from './embeds';
 export * from './interceptors';
-
-const embeds = {
-  simpleEmbed,
-  successEmbed,
-  errorEmbed,
-};
 
 export default {
   embeds,
