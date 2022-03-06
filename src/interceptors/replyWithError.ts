3b84dd3f7692be2d13e6539cdaaa07d24980cfec
DeathVenom54 <fdragon602@gmail.com>
2022-03-06 11:45:13 +0530

    make ephemeral false by default

--- src/interceptors/replyWithError.ts
+++ src/interceptors/replyWithError.ts
@@ -5,14 +5,14 @@ import { errorEmbed } from '../embeds';
  * Replies to an interaction with an error embed
  * @param {BaseCommandInteraction} interaction The interaction to reply to
  * @param {string} errorMessage The error message to send
- * @param {boolean} ephemeral Whether to make reply ephemeral
+ * @param {boolean} ephemeral Whether to make reply ephemeral (default: false)
  * @param {string} details Optional details to include in the message
  * @param {EmojiResolvable} emoji Emoji to include in the title
  */
 export const replyWithError = async (
   interaction: BaseCommandInteraction,
   errorMessage: string,
-  ephemeral: boolean,
+  ephemeral: boolean = false,
   details?: string,
   emoji?: EmojiResolvable,
 ): Promise<void> => {
--- src/interceptors/replyWithSuccess.ts
+++ src/interceptors/replyWithSuccess.ts
@@ -5,14 +5,14 @@ import { successEmbed } from '../embeds';
  * Replies to an interaction with a success embed
  * @param {BaseCommandInteraction} interaction The interaction to reply to
  * @param {string} message The success message to display
- * @param {boolean} ephemeral Whether to make reply ephemeral
+ * @param {boolean} ephemeral Whether to make reply ephemeral (default: false)
  * @param {string} details Optional details to include in the message
  * @param {EmojiResolvable} emoji Emoji to include in the title
  */
 export const replyWithSuccess = async (
   interaction: BaseCommandInteraction,
   message: string,
-  ephemeral: boolean,
+  ephemeral: boolean = false,
   details?: string,
   emoji?: EmojiResolvable,
 ): Promise<void> => {
